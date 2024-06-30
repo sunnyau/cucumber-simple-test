fdd862d5d8e3fb869fbbb96160c2f2611e05c1ce
sunnyau <sunny.au@outlook.com>
2024-06-30 09:28:33 +0100

    upgrade to Java 17 and newer cucumber so that it can run

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,6 +1,9 @@
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
+// import cucumber.api.CucumberOptions;
+// import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
